a6ae19768cc8d32e048270771b779637ef257af6
feiwang <feiwangsioc@gmail.com>
2019-12-18 00:05:26 +0000

    cancel mail

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,6 +1,4 @@
 CarrierWave.configure do |config|
-<<<<<<< HEAD
-
     config.fog_provider = 'fog/aws'                  
     config.fog_credentials = {
       provider:              'AWS',                        
@@ -16,14 +14,3 @@ CarrierWave.configure do |config|
 
 
 end
-=======
-  config.fog_provider = 'fog/aws'
-  config.fog_credentials = {
-    provider:              'AWS',
-    aws_access_key_id:     ENV['S3_KEY'],
-    aws_secret_access_key: ENV['S3_SECRET'],
-    region:                ENV['S3_REGION'],
-  }
-  config.fog_directory  = ENV['S3_BUCKET']
-end 
->>>>>>> 507808c529ded6796ab0db630f544bd4adb31f36
